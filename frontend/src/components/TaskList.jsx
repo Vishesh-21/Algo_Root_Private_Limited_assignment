b1d1eb647b210362ec575a8927739281dfa63bca
Vishu Verma <visheshbaund@gmail.com>
2025-03-26 14:08:23 +0530

    Deploy backend as service on render.

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const apiURI = "http://localhost:3000/tasks/";
+const apiURI =
+  "https://algo-root-private-limited-assignment.onrender.com/tasks";
 
 const TaskList = () => {
   const [tasks, setTasks] = useState([]);
